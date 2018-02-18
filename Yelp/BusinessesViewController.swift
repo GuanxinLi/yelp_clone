7ad870982fcd0cfde895747a6b1180fc59570680
Guanxin <liguanxin078@gmail.com>
2018-02-18 01:54:36 -0800

    layout

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,16 +8,24 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
     
     var businesses: [Business]!
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 120
+        
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
-            
             self.businesses = businesses
+            self.tableView.reloadData()
             if let businesses = businesses {
                 for business in businesses {
                     print(business.name!)
@@ -27,7 +35,26 @@ class BusinessesViewController: UIViewController {
             
             }
         )
+    }
         
+        
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if businesses != nil {
+            return businesses!.count
+        } else {
+            return 0
+        }
+    }
+        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
+            
+        cell.business = businesses[indexPath.row]
+            
+        return cell
+            
+    }
+       
         /* Example of Yelp search with more search options specified
          Business.searchWithTerm("Restaurants", sort: .distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: Error!) -> Void in
          self.businesses = businesses
@@ -39,12 +66,11 @@ class BusinessesViewController: UIViewController {
          }
          */
         
-    }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+        }
     
     /*
      // MARK: - Navigation
@@ -57,3 +83,4 @@ class BusinessesViewController: UIViewController {
      */
     
 }
+
